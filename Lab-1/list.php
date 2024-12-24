784c3da5cdadec2ab12aa57c077ec3247c282bbd
Aarya Shrestha <shresthaaarya404@gmail.com>
2024-12-24 15:15:28 +0545

    AaryaShrestha_20

--- Lab-1/list.php
+++ Lab-1/list.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection
-$conn = new mysqli('localhost', 'root', '', 'aarya_20'); // Adjust database credentials
+$conn = new mysqli('localhost', 'root', '', 'aarya_20'); 
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
